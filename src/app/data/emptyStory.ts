6d2e390bd1c663f4c2f65fba34687c8483dd3fad
jtreeves <jackson.t.reeves@gmail.com>
2022-07-31 12:03:40 -0400

    [service] Add setters and getter to story service

--- src/app/data/emptyStory.ts
+++ src/app/data/emptyStory.ts
@@ -4,8 +4,8 @@ import { Story } from '../interfaces/story';
 export const EMPTY_STORY: Story = {
     mainCharacter: EMPTY_CHARACTER,
     supportingCharacter: EMPTY_CHARACTER,
+    genre: 'comedy',
     actions: [],
     objects: [],
-    genre: 'comedy',
     content: ''
 }
--- src/app/services/story.service.ts
+++ src/app/services/story.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Character } from '../interfaces/character';
+import { Story } from '../interfaces/story';
+import { EMPTY_STORY } from './../data/emptyStory';
 
 @Injectable({
     providedIn: 'root',
 })
 
 export class StoryService {
-    constructor() {}
+    private story: Story = EMPTY_STORY;
+
+    setMainCharacter(character: Character): void {
+        this.story.mainCharacter = character;
+    }
+
+    setSupportingCharacter(character: Character): void {
+        this.story.supportingCharacter = character;
+    }
+
+    setGenre(genre: 'horror' | 'comedy' | 'tragedy' | 'mystery' | 'adventure' | 'romance'): void {
+        this.story.genre = genre;
+    }
+
+    setActions(actions: string[]): void {
+        this.story.actions = actions;
+    }
+
+    setObjects(objects: string[]): void {
+        this.story.objects = objects;
+    }
+
+    setContent(content: string): void {
+        this.story.content = content;
+    }
+
+    getStory(): Story {
+        return this.story;
+    }
 }
