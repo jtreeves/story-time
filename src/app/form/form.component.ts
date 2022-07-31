2bdcd0e2a196795f5f4ddb76e0492e43308992d3
jtreeves <jackson.t.reeves@gmail.com>
2022-07-31 14:35:36 -0400

    [form] Add sections to build main character

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { StoryService } from '../services/story.service';
 import { GENRES } from './../data/genres';
+import { GENDERS } from '../data/genders';
 
 @Component({
     selector: 'app-form',
@@ -16,20 +17,33 @@ export class FormComponent implements OnInit {
     ) {}
 
     genres: string[] = GENRES
+    genders: string[] = GENDERS
 
     storyForm: FormGroup = this.formBuilder.group({
-        genre: new FormControl(
-            '',
-            [
-                Validators.required,
-                Validators.pattern(/(horror|comedy|tragedy|mystery|romance|adventure|^$)/)
-            ]
-        )
+        genre: new FormControl('comedy'),
+        mainCharacterName: new FormControl(''),
+        mainCharacterGender: new FormControl('nonbinary'),
+        mainCharacterOccupation: new FormControl(''),
+        mainCharacterAge: new FormControl(0),
+        mainCharacterAdjective1: new FormControl(''),
+        mainCharacterAdjective2: new FormControl(''),
+        mainCharacterAdjective3: new FormControl(''),
     })
 
     ngOnInit(): void {}
 
     onSubmit(): void {
         this.storyService.setGenre(this.storyForm.value.genre)
+        this.storyService.setMainCharacter({
+            name: this.storyForm.value.mainCharacterName,
+            gender: this.storyForm.value.mainCharacterGender,
+            occupation: this.storyForm.value.mainCharacterOccupation,
+            age: this.storyForm.value.mainCharacterAge,
+            adjectives: [
+                this.storyForm.value.mainCharacterAdjective1,
+                this.storyForm.value.mainCharacterAdjective2,
+                this.storyForm.value.mainCharacterAdjective3
+            ]
+        })
     }
 }
