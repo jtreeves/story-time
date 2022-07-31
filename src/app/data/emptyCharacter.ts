df6b7c7805efa330780446eb26900e151e197a9f
jtreeves <jackson.t.reeves@gmail.com>
2022-07-31 01:56:25 -0400

    [data] Add empty versions of character and story to use for initialization

---
+++ src/app/data/emptyCharacter.ts
@@ -0,0 +1,9 @@
+import { Character } from '../interfaces/character';
+
+export const EMPTY_CHARACTER: Character = {
+    name: '',
+    gender: 'nonbinary',
+    age: 0,
+    occupation: '',
+    adjectives: []
+}
---
+++ src/app/data/emptyStory.ts
@@ -0,0 +1,11 @@
+import { EMPTY_CHARACTER } from './emptyCharacter';
+import { Story } from '../interfaces/story';
+
+export const EMPTY_STORY: Story = {
+    mainCharacter: EMPTY_CHARACTER,
+    supportingCharacter: EMPTY_CHARACTER,
+    actions: [],
+    objects: [],
+    genre: 'comedy',
+    content: ''
+}
